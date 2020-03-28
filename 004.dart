450cffab4aac096c63557429d42687eefc7e3439
ZIN MYO <overmidnight12@gmail.com>
2020-03-27 23:13:12 -0700

    my changes

---
+++ 004.dart
@@ -0,0 +1,11 @@
+void main(){
+   print('something');
+   String greet =greeting();
+   int age      =getAge();
+  
+   print(greet);
+   print(age);
+}
+
+String greeting() => "welcome Thura Aung";
+int getAge()      => 22;
