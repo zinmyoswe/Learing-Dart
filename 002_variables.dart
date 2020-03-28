40309566fd7869c8c7a28c7702b4c098cfa512b3
ZIN MYO <overmidnight12@gmail.com>
2020-03-27 23:02:42 -0700

    my changes

---
+++ 002_variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  int age = 22;
+  print(age);
+  
+  String name = "zayarmyat";
+  print(name);
+  
+  String fname = "Thura";
+  String lname = "Aung";
+  String fullname = fname+" "+ lname;
+  
+  print(fullname);
+  
+  bool isNight = false;
+  print(isNight);
+  
+  dynamic position = "developer";
+  position = 235;
+  print(position);
+  
+  var myNo = 23.5;
+  print(myNo);
+}
